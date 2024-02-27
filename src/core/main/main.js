3c4e26de5b221b13ab2b990684793ef449eee934
samuellipovetsky <samuellpvt@gmail.com>
2024-02-27 13:30:20 -0300

    front-end stuf

--- src/core/main/main.js
+++ src/core/main/main.js
@@ -186,19 +186,26 @@ function Main() {
   return (
 
     <div className="wrapper">
-
+     
       <div className="child matrix-div">
-        <div className='config'>
-          <ConfigContext.Provider value={{ isPlaying, setIsPlaying, drawData, setDrawData }}>
-            <MatrixConfigs></MatrixConfigs>
-          </ConfigContext.Provider>
-        </div>
-        <div className='matrix'>
-          <Matrix qTable={qTable} initialData={matrixData} drawData={drawData} updateMatrix={updateMatrix} />
-        </div>
 
+          <div className='config'>
+            <ConfigContext.Provider value={{ isPlaying, setIsPlaying, drawData, setDrawData }}>
+              <MatrixConfigs></MatrixConfigs>
+            </ConfigContext.Provider>
+          </div>
+          <div className='matrix'>
+            <Matrix qTable={qTable} initialData={matrixData} drawData={drawData} updateMatrix={updateMatrix} />
+          </div>
+          <div className='filler'></div>
+          <div className='matrix-controls'>
+          <MatrixControls isPlaying={isPlaying} resetTable={resetTable} runQlearning={runQlearning} updateIsPlaying={updateIsPlaying} ></MatrixControls>
+          </div>
+        
 
+          
       </div>
+
       <div className="child graph ">
 
         <ResponsiveContainer width="100%">
--- src/core/matrixControls/matrixControls.js
+++ src/core/matrixControls/matrixControls.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import "./matrixControls.css"
-
+import { FaPlay } from "react-icons/fa";
+import { FaPause } from "react-icons/fa";
+import { MdOutlineRestartAlt } from "react-icons/md";
 function MatrixControls(props) {
+
 
     let playingStatus
     if (props.isPlaying == true) {
@@ -14,24 +17,42 @@ function MatrixControls(props) {
         props.updateIsPlaying()
 
     }
-    const handleRunQlearning =()=>{
+    const handleRunQlearning = () => {
         props.runQlearning(1000)
     }
-    const handleResetTable =()=>{
+    const handleResetTable = () => {
         props.resetTable()
     }
 
+
     return (
         <div className='matrix-control'>
-            <button onClick={handleUpdateIsPlaying}>
-                play
-            </button>
-            <button onClick={handleRunQlearning}>
-                Treinar por mais 1000 passos
-            </button>
-            <button onClick={handleResetTable}>
-                Resetar treinamento
-            </button>
+            <div  className="div-control" onClick={handleUpdateIsPlaying}>
+                <div>
+                {props.isPlaying ? <FaPause size={30}></FaPause> : <FaPlay size={30} />}
+                </div>
+                <div>
+                    {props.isPlaying?<p>pausar</p>:<p>iniciar</p>}
+                </div>
+                
+            </div>
+            <div>
+                <button style={{ width: "100%" }} onClick={handleRunQlearning}>
+                    Treinar por mais 1000 passos
+                </button>
+            </div>
+            <div className="div-control" onClick={handleResetTable} >
+                <div style={{ width: "100%" }} >
+                    <MdOutlineRestartAlt size={30} />
+                </div>
+                <div>
+                    Resetar 
+                </div>
+            </div>
+            <div>
+
+            </div>
+
         </div>
 
     );
